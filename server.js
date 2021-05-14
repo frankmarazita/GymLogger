d3dfc85223f4d131bd857f090c1d7f93d47347f3
Frank Marazita <fmarazita1@gmail.com>
2021-05-15 00:01:10 +1000

    Separated out app functions

--- server.js
+++ server.js
@@ -1,84 +1,8 @@
-const config = require('./config/config')
-
-const development = process.env.NODE_ENV === 'development';
-
-const port = parseInt(process.env.PORT);
-
-const express = require('express');
-const session = require('express-session');
-const handlebars_express = require('express-handlebars');
-const helmet = require("helmet");
-
-// Import Database
+const app = require('./app');
 const db = require('./controllers/db');
-
-// Initialise Express
-const app = express();
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    key: process.env.SESSION_KEY,
-    resave: true,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        secure: !development,
-        // domain: 'example.com'
-    }
-}));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
-// Initialise Handlebars
-const handlebars = handlebars_express.create({
-    extname: 'hbs',
-    layoutsDir: __dirname + '/views/layouts',
-    partialsDir: __dirname + '/views/partials',
-    // defaultLayout: 'default',
-    helpers: require('./handlers/handlebars_helpers.js')
-});
-
-app.set('view engine', 'hbs');
-app.engine('hbs', handlebars.engine);
-
-// Initialise Helmet
-app.use(
-    helmet({
-        contentSecurityPolicy: {
-            directives: {
-                defaultSrc: [
-                    "'self'"
-                ],
-                scriptSrc: [
-                    "'self'",
-                    "https://cdnjs.cloudflare.com",
-                    "https://www.gstatic.com",
-                    "'unsafe-inline'" // Avoid this
-                ],
-                connectSrc: [
-                    "'self'",
-                ],
-                styleSrc: [
-                    "'self'",
-                    "https://stackpath.bootstrapcdn.com",
-                    "https://maxcdn.bootstrapcdn.com",
-                    "https://maxst.icons8.com",
-                    "https://www.gstatic.com",
-                    "'unsafe-inline'" // Avoid this
-                ],
-                fontSrc: [
-                    "'self'",
-                    "https://maxcdn.bootstrapcdn.com",
-                    "https://maxst.icons8.com"
-                ],
-                imgSrc: [
-                    "'self'",
-                    "data:" // Avoid this
-                ]
-            }
-        },
-    })
-);
+const auth = require('./controllers/auth');
+const error = require('./controllers/error');
+const port = parseInt(process.env.PORT);
 
 // Routes
 require('./routes/auth')(app, db);
@@ -91,9 +15,6 @@ require('./routes/group')(app, db);
 require('./routes/exercise')(app, db);
 require('./routes/exercise_data')(app, db);
 require('./routes/weight')(app, db);
-
-const auth = require('./controllers/auth');
-const error = require('./controllers/error');
 
 // Default Route
 app.use(function (req, res) {
