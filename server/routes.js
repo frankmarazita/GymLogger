eb1127db166ed04c6d849b4c393309ab946bd186
Frank Marazita <fmarazita1@gmail.com>
2021-10-17 17:48:02 +1100

    Route navigation logging in dev

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,16 @@
+const app = require('./app')
+
+const CT = require('./constants/codeTables')
+
+const IS_DEVELOPMENT = process.env.NODE_ENV === CT.System.C.Development
+
+if (IS_DEVELOPMENT) {
+    app.use(function (req, res, next) {
+        console.log(req.path)
+        next()
+    })
+}
+
 require('./routes/exercises.routes')()
 require('./routes/groups.routes')()
 require('./routes/login.routes')()
