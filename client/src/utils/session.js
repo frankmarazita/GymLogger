b88a7edb944be7921c0487b8e3f0ba08c24e8856
Frank Marazita <fmarazita1@gmail.com>
2021-10-24 20:54:09 +1100

    Added favicon and fixed react build

--- client/src/utils/session.js
+++ client/src/utils/session.js
@@ -1,29 +1,19 @@
-const jwt = require('./jwt');
-
 const SESSION_ATTRIBUTE_NAME = 'token';
 
-module.exports = {
+let session = {
 
-    setToken(token) {
+    setToken: function (token) {
         return sessionStorage.setItem(SESSION_ATTRIBUTE_NAME, token)
     },
 
-    getToken() {
+    getToken: function () {
         return sessionStorage.getItem(SESSION_ATTRIBUTE_NAME)
     },
 
-    deleteToken() {
+    deleteToken: function () {
         return sessionStorage.removeItem(SESSION_ATTRIBUTE_NAME)
-    },
-
-    async isLoggedIn() {
-        let token = sessionStorage.getItem(SESSION_ATTRIBUTE_NAME)
-        let decoded = await jwt.decode(token)
-        if (decoded) {
-            console.log(decoded)
-            return true
-        }
-        return false
     }
 
 }
+
+export default session;
